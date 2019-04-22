9f7f331d7fa5c7d39261dca71d907bcfe7a7babe
Inna Mon <inna.mon@gmail.com>
2019-04-22 11:59:13 -0500

    added Courier font to index.css and added Rank Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
