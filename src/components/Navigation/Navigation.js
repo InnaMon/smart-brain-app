8ad14f3af5fe351bbb0fe33332caa28475d3eca6
Inna Mon <inna.mon@gmail.com>
2019-04-22 10:45:03 -0500

    added styles to nav and linear gradient to index.css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end', paddingRight: '20px'}}> 
-            <p>Sign Out</p>
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}> 
+            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
     );
 }
