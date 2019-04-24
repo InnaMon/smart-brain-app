4a19961e95db70b1deef11872b5bf6aeeb31320e
Inna Mon <inna.mon@gmail.com>
2019-04-23 20:18:50 -0500

    added function to sign in component of Signin page to redirect to home page upon hitting the sign in button

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
   }
 
@@ -67,6 +68,10 @@ class App extends Component {
     .catch(err => console.log('Error', err));
   }
 
+  onRouteChange = () => {
+    this.setState({route: 'home'});
+  }
+
   render() {
     return (
       <div className="App">
@@ -74,14 +79,18 @@ class App extends Component {
           params={particlesOptions}
         />
         <Navigation />
-          <Signin />
-          <Logo />
-          <Rank />
-          <ImageLinkForm 
-            onInputChange={this.onInputChange} 
-            onButtonSubmit={this.onButtonSubmit}
-          />
-          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+          { this.state.route === 'signin' 
+          ? <Signin onRouteChange={this.onRouteChange}/>
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+            </div>
+        }
       </div>
     );
   }
