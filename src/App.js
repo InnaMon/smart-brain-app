9bdafe0980c54644363968976e3dcd5d1e720167
Inna Mon <inna.mon@gmail.com>
2020-08-12 19:23:35 -0500

    added initialState variable to refresh state after old user logs out and new user signs in

--- src/App.js
+++ src/App.js
@@ -21,39 +21,39 @@ const particlesOptions = {
   }
 }
 
-// const initialState = {
-//   input: '',
-//   imageUrl: '',
-//   box: {},
-//   route: 'signin',
-//   isSignedIn: false,
-//   user: {
-//     id: '',
-//     name: '',
-//     email: '',
-//     entries: 0,
-//     joined: ''
-//   }
-// }
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    // this.state = this.initialState;
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
+    // this.state = {
+    //   input: '',
+    //   imageUrl: '',
+    //   box: {},
+    //   route: 'signin',
+    //   isSignedIn: false,
+    //   user: {
+    //     id: '',
+    //     name: '',
+    //     email: '',
+    //     entries: 0,
+    //     joined: ''
+    //   }
+    // }
   }
 
   loadUser = (data) => {
@@ -120,8 +120,8 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      // this.setState({initialState})
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
+      // this.setState({isSignedIn: false})
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
