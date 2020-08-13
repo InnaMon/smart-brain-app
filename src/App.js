b6645717fba6196e587997d77777529ba6a01261
Inna Mon <inna.mon@gmail.com>
2020-08-13 14:49:34 -0500

    prevent img submission if no input available

--- src/App.js
+++ src/App.js
@@ -74,34 +74,36 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    fetch('https://fast-plateau-16833.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        input: this.state.input
+    if (this.state.input) {
+      this.setState({imageUrl: this.state.input});
+      fetch('https://fast-plateau-16833.herokuapp.com/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
       })
-    })
-    .then(response => response.json())
-    .then (response => {
-      if (response) {
-        fetch('https://fast-plateau-16833.herokuapp.com/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id
+      .then(response => response.json())
+      .then (response => {
+        if (response) {
+          fetch('https://fast-plateau-16833.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
           })
-        })
-        .then(response => response.json())
-        .then(count => {
-          this.setState(Object.assign(this.state.user, { entries: count }))
-        })
-        .catch(console.log);
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+          .catch(console.log);
 
-      }
-      this.displayFaceBox(this.calculateFaceLocation(response))
-    })
-    .catch(err => console.log('Error', err));
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log('Error', err));
+    }
   }
 
   onRouteChange = (route) => {
