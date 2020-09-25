cedcf59ff4c40cb47319ca6f896f83d5e20df4de
Inna Mon <inna.mon@gmail.com>
2020-09-25 02:42:02 -0500

    added information icon to clarify URL type for better user experience

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ImageLinkForm.css';
+import InformationIcon from '../../assets/information.svg';
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
@@ -10,10 +11,18 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
               <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
                   <input 
-                    className='f4 pa2 w-70 center' 
-                    type='tex' 
+                    className='f4 pa2 w-70 center'
+                    style={{paddingLeft: '30px'}} 
+                    type='text' 
+                    placeholder="Image URL goes here..."
                     onChange={onInputChange}
                   />
+                  <div className="information-icon">
+                      <div className="tooltip">
+                      <img className="information-icon-img" src={InformationIcon} alt="information icon" />
+                          <span className="tooltiptext">Make sure your URL ends with an image extension like jpeg or png</span>
+                      </div>
+                  </div>
                   <button 
                     className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                     onClick={onButtonSubmit}
